762767e6525b262e45cce30e96d2ab8bc01bd169
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-22 14:55:16 +0530

    REFACTORED: Ride input as ArrayList

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -2,18 +2,20 @@ package com.bridgelabz.cabinvoicegenerator;
 
 import java.util.ArrayList;
 
-public class InvoiceGenerator {
+public class InvoiceGenerator implements InvoiceGeneratorIF{
 	
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5;
 
+	@Override
 	public double calculateFare(double distance, int time) {
 		
 		double totalFare = distance*MINIMUM_COST_PER_KILOMETER + time*COST_PER_TIME;
 		return Math.max(totalFare, MINIMUM_FARE);
 	}
-
+	
+	@Override
 	public double calculateFare(Ride[] rides) {
 		
 		double totalFare = 0;
@@ -23,6 +25,7 @@ public class InvoiceGenerator {
 		return totalFare;
 	}
 	
+	@Override
 	public InvoiceSummary calculateFareReturnObject(ArrayList<Ride> rides) {
 		
 		double totalFare = 0;
